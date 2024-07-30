a064e7559373bdafdbdb54a99505842a7fc5010d
Matheus <matheus.mgrbq@gmail.com>
2024-07-30 08:57:05 -0300

    writing text to files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,22 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String[] lines = new String[] {
+                "Good morning",
+                "Good afternoon",
+                "Good evening",
+                "Good night"
+        };
+        String path = "C:\\Users\\opah\\Desktop\\JAVA studies\\java-io\\greetings.txt";
 
-        String path = "C:\\Users\\opah\\Desktop\\JAVA studies\\java-io\\in.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(path)))
-        {
-            String line = br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
         }
         catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
