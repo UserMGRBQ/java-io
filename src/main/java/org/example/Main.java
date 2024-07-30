c5e9c8cc6a518b3e7476637cd7b42a808a4e7b70
Matheus <matheus.mgrbq@gmail.com>
2024-07-30 08:41:59 -0300

    reading text from txt file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test!");
+
+        String path = "C:\\Users\\opah\\Desktop\\JAVA studies\\java-io\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path)))
+        {
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
